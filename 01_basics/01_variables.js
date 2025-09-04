1f838e6ca326b322610c63d1318e82ed8d655175
RubiyaHud <32358190+RubiyaHud@users.noreply.github.com>
2025-09-04 06:34:18 +0000

    class-4

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 123456
+let accountEmail = "r@gmail.com"
+var accountPassword = "123"
+accountCity = "Dhaka"
+let accountState;
+
+console.log("varibales in table format:")
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState])
