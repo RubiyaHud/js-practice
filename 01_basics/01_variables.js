935e1af239d0c6b0cf793b8b189b8be396a82150
RubiyaHud <32358190+RubiyaHud@users.noreply.github.com>
2025-09-04 08:39:46 +0000

    class - 4, Notes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,14 @@
+/** NOTE:
+ * Prefer not to use var
+ * becuase of issue in Block Scope and Functional Scope
+ */
+
 const accountId = 123456
 let accountEmail = "r@gmail.com"
 var accountPassword = "123"
 accountCity = "Dhaka"
 let accountState;
+
 
 console.log("varibales in table format:")
 console.table([accountId, accountEmail,accountPassword,accountCity,accountState])
