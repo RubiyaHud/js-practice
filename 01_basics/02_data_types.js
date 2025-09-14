6c2861260b6652fc428671303e3bba980b7d0ce8
RubiyaHud <32358190+RubiyaHud@users.noreply.github.com>
2025-09-14 07:00:42 +0000

    datatypes

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,20 @@
+"use strict"; // Treat all JS code as Newer version
+//alert(3 + 3); //alert() statement, it will make error here, because we are using node.js, NOT browser!
+
+/**-------------Primitive Data Types ----------- */
+//number => 2 to power 53
+//bigint => big numeric value
+//string => ""
+//boolean => true/false
+//null => standalone value, or empty value
+//undefined => variable is declared but value is not assigned yet
+
+
+/**----------- Non-Primitive Datatypes -------------------*/
+//Object
+
+console.log(typeof value); //output:undefined
+//let value = 10; it is an error with 'let'
+var value = 10;
+console.log(typeof value);//output:number
+console.log(typeof null);//output:object, a historical bug in javascript
